d0cb00268839449b8ff99a1225c18194d8f212eb
Cristian Rivera <cristian.rivera16@gmail.com>
2016-03-23 10:34:42 -0400

    Update about.blade.php

--- partials/about.blade.php
+++ partials/about.blade.php
@@ -22,38 +22,13 @@
 
 		<div class="cite-area breathing-space">
 			<h3>Cite:</h3>
-			Citing SEAGrid in academic papers helps us show impact. If you use SEAGrid in your research please help by including the following citation information and acknowledgement in your paper.
-			<p>SEAGrid (<a href="http://www.seagrid.org">http:www.seagrid.org</a>)[*] is acknowledged for computational resources and services for the selected results used in this publication. </p>
-			<p>* (a) N. Shen, Y. Fan, S. Pamidighantam, E-Science Infrastructures for Molecular Modeling and Parametrization, Journal of Computational Science 5 (2014), pp. 576-589; DOI information: 10.1016/j.jocs.2014.01.005</p>
-			<p> (b) R. Dooley, K. Milfeld, C. Guiang, S. Pamidighantam, G. Allen, From Proposal to Production: Lessons Learned Developing the Computational Chemistry Grid Cyberinfrastructure, Journal of Grid Computing, 2006, 4, 195-208. </p>
-			<p> (c) R. Dooley, G. Allen, and S. Pamidighantam. "Computational Chemistry Grid: Production Cyberinfrastructure for Computational Chemistry". Proceedings of the 13th Annual Mardi Gras Conference, Baton Rouge, LA, Feb. 2005, pg 83. </p>
-			<p> (d) K. Milfeld, C. Guiang, S. Pamidighantam, J. Giuliani. Cluster Computing through an Application-oriented Computational Chemistry Grid. Proceedings of the 2005 Linux Clusters: The HPC Revolution, Apr. 2005. </p>
-			<p> (e) This work used the Extreme Science and Engineering Discovery Environment (XSEDE), which is supported by National Science Foundation grant number OCI-1053575.</p>
-	 		<a target="_blank" href="{{URL::to('/')}}/themes/{{Session::get('theme')}}/assets/app/BibTex.txt">Get BibTex</a>
+			Citing PHASTA in academic papers helps us show impact. If you use PHASTA in your research please help by including the following citation information and acknowledgement in your paper.
+			<p>Whiting, Christian H., and Kenneth E. Jansen. "A stabilized finite element method for the incompressible Navier-Stokes equations using a hierarchical basis." International Journal for Numerical Methods in Fluids 35.1 (2001): 93-116.</p>
 		</div>
 	</div>
 
 <hr class="separation-line blue-bg"/>
 </div>
 
-<div class="col-md-12">
-
-<h3>Mission Statement</h3>
-<p> Enable computational and experimental scientists to do "more" computational chemistry by providing capability computing resources and services at their fingertips  </p>
-<hr class="separation-line blue-bg"/>
-
-<h3>Project Summary</h3>
-<p> The term "cyberinfrastructure" now describes the varied computational resources and services, both interconnected and distributed, supporting scientific and engineering research. Great progress has been made in developing the necessary cyberinfrastructure for large-scale science and engineering projects. However, a larger research community with smaller, individual projects does not currently enjoy convenient access to the cyberinfrastructure. We now stand at a point in time at which cyberinfrastructure has matured sufficiently to allow economical integration and deployment to a larger, more diverse research community. In this proposal we describe a plan to do exactly that for the computational chemistry community - a very large community dominated by individual investigators and small groups - that has been making use of significant amounts of NSF funded resources for many years representing one of the largest components in the job mix of all supercomputer centers. This is because of the importance of computational chemistry to the fundamentals of materials science and biology as well as chemistry. Any branch of research which depends on the understanding of molecular structure and function is going to benefit from having access to adequate computational resources. The computational chemistry community, ranging from theorists to experimentalists, has a growing need for computational resources. The opportunity exists to enhance the cyberinfrastructure with an easy-to-use interface that lowers the barrier to use far beyond anything imagined to date and provides that interface to the entire community. This project leverages the many efforts supported by the NSF and other agencies that built the cyberinfrastructure to now allow us to deliver service to a research community via a desktop environment coupled to grid computing. </p>
-<hr class="separation-line blue-bg"/>
-
-<h3>Intellectual Merit</h3>
-<p> The significant problems to address in the grid computing environment include: security, job tracking, diverse licensing agreements, variable resource allocations, extremely large data sets, and the desktop user interface. Some progress has been made, for example GridCore in Europe with job tracking, but significant challenges remain for the other issues. Herein, we bring together cyberinfrastructure resources from Ohio Supercomputing Center, Univ. of Kentucky, NCSA, Texas Advanced Computing Center, and LSU along with testing and assessment from representative computational chemistry groups and will be an exemplar for deployment practice for similar communities. </p>
-<hr class="separation-line blue-bg"/>
-
-<h3>Broader Impact</h3>
-<p> This project empowers the broader chemistry community with access to grid computing. The team proposing this work includes both technologists who have long been involved in the activities of high-end computing, grid infrastructure, collaboration tools, and other technologies – precursors of the emerging cyberinfrastructure – and chemistry researchers specializing in the use of computational tools to further their research goals. This project will not only support advances in chemistry but will be a leading example for deploying such capabilities to other scientific and engineering research communities. Further, these capabilities have enormous potential impact to those industries which generate new materials so important to the electronic, biotechnology and medical sectors. One has only to think of the development of nanotechnology devices and pharmaceuticals to realize how important computational chemistry is to the country's economy and the availability of integrated technologies to support their development.   </p>
-<hr class="separation-line blue-bg"/>
-
-</div>
 
 </div>
