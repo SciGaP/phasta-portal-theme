6d4419bcdd725cf18ffdb9dc208c3038e43dfa08
Cameron Smith <smithc11@rpi.edu>
2016-04-26 09:01:07 -0400

    basic project, experiment and application instructions

--- partials/documentation.blade.php
+++ partials/documentation.blade.php
@@ -4,6 +4,18 @@
 <h2 class="text-center">Training and Documentation</h2>
 <hr class="separation-line blue-bg"/>
 
+<h3 class="text-center">Getting Started</h3>
+    <p>1. Create a new project by clicking Project-\>Create .</p>
+    <p>2. Enter a project name and description.</p>
+    <p>3. Create an experiment by clicking Experiment->Create . Note, multiple experiments can be created in a single project.</p>
+    <p>4. Enter an experiment name and description and select a project.  The 'Application' should be set to 'Phasta_P'. Click 'Continue'.</p>
+    <p>5. In the Application input frame use the 'Browse...' buttons to upload the input files for the analysis.  The off-center annular flow case files <a href="http://www.scorec.rpi.edu/~cwsmith/phastaPartialSlip/Annular-Off-Center/">here</a> will be used for demonstration purposes.  The 'geom.xmt_txt' is the Parasolid-Geometric-Model, geomAtts.smd contains the Problem-Definition, geom.sms is the Mesh-Description-File, and solver.inp is the Solver-Input-Parameters file.</p>
+    <p>6. Set Compute Resource to 'stampede.tacc.xsede.org', the queue to 'development', Node Count to '1', Total Core Count to '16', Wall Time Limit to '30 minutes', and leave Total Physical Memory blank.</p>
+    <p>7. Under Notifications click the check box to job status enable email updates.</p>
+    <p>8. Click 'Save and launch'.</p>
+
+
+<h3 class="text-center">Additional Documentation</h3>
     <p>See <a target="_blank" href="https://wiki.scorec.rpi.edu/wiki/PHASTA_Documentation">here</a> for course notes and tutorial videos and papers.</p>
 
 </div>
